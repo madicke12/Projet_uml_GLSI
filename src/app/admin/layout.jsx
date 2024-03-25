c97e95ecf67cd68a738dbe029548410a8b3d008c
madicke12 <cissemadicke8@gmail.com>
2024-03-25 21:35:11 +0000

    madicke's commit

---
+++ src/app/admin/layout.jsx
@@ -0,0 +1,21 @@
+
+
+import SideNav from "@/components/sidenav";
+const layout = (props) => {
+  return (
+  
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden dark:bg-zinc-700">
+      <div className="w-full md:flex-none md:w-64 hidden md:block py-3">
+       <SideNav/>
+      </div>
+      <div className="w-full h-full ">
+        <div className="h-full md:p-3 ">
+          {props.children}      
+          </div>
+      </div>
+    </div>
+    
+  );
+};
+
+export default layout;
